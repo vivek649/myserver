56dfc796e48164142de4d1d412bfb633894d5345
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-05-14 01:44:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -33,7 +33,61 @@ app.get("/hack", async (req, res) => {
 
   res.send({ title, thumbnail, audioFormats, formats });
 });
+app.get('/download', async (req, res) => {
 
+  try {
+
+    const {
+
+      URL, downloadFormat, quality, title,
+
+    } = req.query;
+
+    if (downloadFormat === 'audio-only') {
+
+      res.setHeader(
+
+        'Content-Disposition',
+
+        `attachment; filename=${title.substring(0, 40)}.mp3`,
+
+      );
+
+      ytdl(URL, {
+
+        filter: format => format.container === 'm4a' && !format.encoding,
+
+        quality: quality === 'high' ? 'highest' : 'lowest',
+
+      }).pipe(res);
+
+    } else {
+
+      res.header(
+
+        'Content-Disposition',
+
+        `attachment; filename="${title.substring(0, 25)}.mp4"`,
+
+      );
+
+      ytdl(URL, {
+
+        filter: downloadFormat === 'video-only' ? 'videoonly' : 'audioandvideo',
+
+        quality: quality === 'high' ? 'highestvideo' : 'lowestvideo',
+
+      }).pipe(res);
+
+    }
+
+  } catch (e) {
+
+    console.log(e);
+
+  }
+
+});
 app.get("/videodl", async (req, res) => {
   const url = req.query.url;
   const itag = req.query.itag;
